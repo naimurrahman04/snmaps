4a6b2ad971245417995c2ad8b898e985b397fa38
meepmaster360 <paulomotaborges@gmail.com>
2022-05-04 20:42:35 +0100

    2

--- iLping.sh
+++ iLping.sh
@@ -3,9 +3,7 @@
 # Ping ip
 
 for ip in `seq 1 254` ; do
-
 ping -c 1 $1.$ip | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" &
-
 done
 
 
--- snmaps.sh
+++ snmaps.sh
@@ -10,17 +10,25 @@ RED="\033[1;31m"
 GREEN="\033[1;32m"
 NOCOLOR="\033[0m"
 
+# Nmap installation	
+	if [ ! -x "$(command -v nmap)" ];then
+        echo -e "\n${RED}[+]${NOCOLOR} nmap not detected...Installing\n"
+        sudo apt-get install nmap -y > installing;rm installing
+	else
+    echo -e "${GREEN}[+]${NOCOLOR} nmap detected\n"
+     
+	fi
+
 # Confirmation of IP address and nmap it
+    if [ "$1" == "" ]
+        then
+        echo "\n$GREEN}you forgot an IP address!${NOCOLOR}"
+        echo "$GREEN}Syntax: ./snmaps.sh 192.168.0${NOCOLOR}\n"
 
-if [ "$1" == "" ]
-    then
-    echo "\n$GREEN}you forgot an IP address!${NOCOLOR}"
-    echo "$GREEN}Syntax: ./snmaps.sh 192.168.0${NOCOLOR}\n"
+        else
+        ./iLping.sh $1 >ip.txt
 
-    else
-    ./iLping.sh $1 >ip.txt
-
-    for ip in $(cat ip.txt);do 
-    sudo nmap -T4 -A -p- -O $ip; 
-    done 
+            for ip in $(cat ip.txt);do 
+            sudo nmap -T4 -A -p- -O $ip; 
+            done 
 fi
